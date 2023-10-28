79e7e13e210a311a410a692545883ab6e9c03f3c
Diana Rand <diana.rand@outlook.com>
2023-10-28 16:59:48 +0300

    initial commit for vue

---
+++ src/views/RaidSignup.vue
@@ -0,0 +1,13 @@
+<template>
+  <meta http-equiv="refresh" content="0; url='https://docs.google.com/spreadsheets/d/1bS8tYyynSgXWVKyBu4gbZubbajSNyGfdYgD-Sav4GyE/edit#gid=0'" />
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
